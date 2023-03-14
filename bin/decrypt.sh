efe047daaf632e3a15f6af6108da2c15aeb38ae3
Tristan Smith <trystendsmyth@gmail.com>
2023-03-14 03:04:35 -0400

    Fix bin files, path, and add work profile

--- bin/decrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for file in `find . -name '*.enc'`; do
-  target=${file%%.enc}
-  echo "Decrypting '$file' to '$target'..."
-  openssl enc -d -aes-256-cbc -in "$file" -out "$target"
-done
